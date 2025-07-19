00e3bfc017e7c4b90ad8b526e7b087e6755f5447
bzswaims-school <bzswaims@uw.edu>
2025-07-19 16:07:05 -0700

    Theta should be done, mystate should be adjusted, ive decided to leave it incase someone else has adjusted it

--- Cryptography/Part1/src/KECCAK_F.java
+++ Cryptography/Part1/src/KECCAK_F.java
@@ -1,12 +1,13 @@
 class KECCAK_F {
 
 
+    //TODO: ADJUST THIS TO BE A 3D ARRAY OF LENGTH 5X5X(64/BLOCKSIZETHINGY)
     private static byte[] MY_STATE;
 
     KECCAK_F(){}
 
 
-    public static void permutate(final byte[] theState) {
+    public static void permutate(final byte [] theState) {
         MY_STATE = theState;
         for(int i = 0; i < 24; i++) {
             theta();
@@ -21,7 +22,57 @@ class KECCAK_F {
      * MixColumns
      */
     private static void theta() {
-        byte[] temp = MY_STATE;
+        int laneLength = MY_STATE[0][0].length;
+
+        byte[][] C = new byte [5][laneLength];
+        byte[][] D = new byte [5][laneLength];
+
+        //filling c with 0
+        for(int x = 0; x < 5; x++)
+        {
+            for(int z = 0; z < 5; z++)
+            {
+                C[x][z] = 0;
+            }
+        }
+
+        //Computing C[x][z]
+        //C[x,z]=A[x, 0,z] ⊕ A[x, 1,z] ⊕ A[x, 2,z] ⊕ A[x, 3,z] ⊕ A[x, 4,z]
+        for (int x = 0; x < 5; x++)
+        {
+            for(int y = 0; y < 5; y++)
+            {
+                for (int z = 0; z < laneLength; z++)
+                {
+                    C[x][z] ^= MY_STATE[x][y][z];
+                }
+            }
+        }
+
+        //Computing D[x][z]
+        //D[x,z]=C[(x-1) mod 5, z] ⊕ C[(x+1) mod 5, (z –1) mod w].
+        for(int x = 0; x < 5; x++)
+        {
+            for(int z = 0; z < laneLength; z++)
+            {
+                //(x+4)%5 equivalent (x-1)%5 and covers the wrap around
+                //adding the laneLength to z at the end helps cover negative values as well
+                D[x][z] = (byte) (C[(x + 4) % 5][z] ^ C[(x + 1) % 5][(z + laneLength- 1) % laneLength]);
+            }
+        }
+
+        //Computing A'[x][y][z]
+        //A′[x, y,z] = A[x, y,z] ⊕ D[x,z].
+        for(int x = 0; x < 5; x++)
+        {
+            for(int y = 0; y < 5; y++)
+            {
+                for(int z = 0; z < laneLength; z++)
+                {
+                    MY_STATE[x][y][z] ^= D[x][z];
+                }
+            }
+        }
     }
 
     /**
